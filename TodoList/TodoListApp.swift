f52f2442bf56129a06149d4d33955690ec8951ce
Jason Bao <baihongbao@outlook.com>
2024-02-07 13:42:45 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jason on 2024/2/7.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
