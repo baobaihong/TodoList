4dfc3ff4ebff8eb9c62a3aa6daebcded3cdfba1d
Jason Bao <baihongbao@outlook.com>
2024-02-09 20:31:30 +0800

    No item View, animation and transition

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,69 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Jason on 2024/2/9.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Click the Add button to add new todo item")
+                    .padding(.bottom, 20)
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("Add Something 🥳")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.horizontal, animate ? 30 : 40)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0,
+                    y: animate ? 50 : 30
+                )
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: DispatchWorkItem(block: {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 1.5)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
+}
