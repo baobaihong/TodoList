4aaf3843f5ec7c1c1eb2dbfd83220259e2f688ff
Jason Bao <baihongbao@outlook.com>
2024-02-09 17:58:45 +0800

    Saving and retriving data to UserDefault by using JSON format

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,19 +8,24 @@
 import Foundation
 
 @Observable class ListViewModel {
-    var items: [ItemModel] = []
+    var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemKeys = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second!", isCompleted: true),
-            ItemModel(title: "Third!", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+        guard 
+            let data = UserDefaults.standard.data(forKey: itemKeys),
+            let newItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        items = newItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -39,6 +44,12 @@ import Foundation
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.setValue(encodedData, forKey: itemKeys)
         }
     }
 }
